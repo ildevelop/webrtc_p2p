b9c3867b985d4051482cb5e681585ed477912ddf
ildevelop <raduilya@gmail.com>
2017-07-01 18:37:50 +0300

    https

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
-var express = require('express.io');
-fs = require('fs');
+fs = require('fs')
 options = {
     key: fs.readFileSync('./key'),
     cert: fs.readFileSync('./cert')
-};
+}
+
+var express = require('express.io');
 var app = express();
-app.https(options).io();
-var PORT = 80;
+// app.http().io();
+app.https(options).io()
+var PORT = 3000;
 console.log('server started on port:' , PORT);
 
 app.use(express.static(__dirname + '/public'));
@@ -17,24 +19,24 @@ app.get('/',(req, res) => {
 
 app.io.route('ready',(req) => {
     req.io.join(req.data.chat_room);
-    req.io.join(req.data.signal_room);
-    app.io.room(req.data).broadcast('announce',{
-        message: 'New client in the ' + req.data + 'room.'
-    })
+req.io.join(req.data.signal_room);
+app.io.room(req.data).broadcast('announce',{
+    message: 'New client in the ' + req.data + 'room.'
+})
 })
 
 app.io.route('send',(req) => {
     app.io.room(req.data.room).broadcast('message', {
-        message: req.data.message,
-        author: req.data.author
-    });
+    message: req.data.message,
+    author: req.data.author
+});
 })
 
 app.io.route('signal',(req) => {
     // Note the use of req for broadcasting so only the sender doest`t their own message
     req.io.room(req.data.room).broadcast('signaling_message', {
-        type: req.data.type,
-        message: req.data.message
-    });
+    type: req.data.type,
+    message: req.data.message
+});
 })
 app.listen(PORT);
